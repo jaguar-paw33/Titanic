f4411a8c1e1911ea5b9007396169ab5f1d3555cb
jaguar-paw33 <mr.priyankmishra@gmail.com>
2020-11-03 00:30:01 +0530

    add style

--- Titanic Prediction.py
+++ Titanic Prediction.py
@@ -12,14 +12,16 @@ from sklearn import preprocessing
 
 
 # Dataset Description
-st.title("Titanic Survival Prediction")
 st.markdown(body='''
+<h1 style='color:#024661; text-align:center;'>Titanic Survival Prediction</h1>
+<br>
 <style>
 body {
 background-image: url("https://cutewallpaper.org/21/gradient-hd-background/Blue-Gradient-Background-4K-HD-Desktop-Wallpaper-for-4K-.jpg");
 background-size: cover;
 background-position:center;
 background-attachment: fixed;
+color:#1f414f;
 }
 </style>
 
@@ -58,6 +60,7 @@ background-attachment: fixed;
    </ol>
    <br>
    <div style='border-bottom:2px Solid Black'></div>
+   <br>
 ''', unsafe_allow_html=True)
 
 #Loading Data
@@ -65,6 +68,7 @@ background-attachment: fixed;
 def load_data():
   data=pd.read_csv('train.csv')
   return data
+
 
 st.header('Loading Dataset')
 data_load_state=st.text('Loading Data...')
@@ -82,11 +86,11 @@ titanic['Age']=titanic['Age'].astype('int64')
 if st.checkbox('Show Dataset'):
   st.subheader('Titanic Survival Dataset')
   st.write(titanic)
-  st.write('Dimensions of the Dataset')
+  st.write('Dimensionality of the Dataset')
   st.write(titanic.shape)
-  st.write("Description of Dataset")
+  st.write("Description of the Dataset")
   st.write(titanic.describe())
-  st.write("Number of Survived(1) and Died(0) people in the dataset")
+  st.write("Number of Survived(1) and Died(0) passengers")
   st.write(titanic['Survived'].value_counts())
 
   #Exploratory Data Analysis
@@ -97,20 +101,103 @@ if st.checkbox('Show Dataset'):
   ''', unsafe_allow_html=True)
 
   if(st.checkbox('Show Plots')):
+    custom_pallete=['#13a9d6','#6cd5f5','#a3d3e3']
+    sns.set_style("darkgrid")
+    sns.set_context("paper")
+    sns.despine()
+    sns.set_palette(custom_pallete)
+
     fig1,ax1=plt.subplots()
-    ax1.set_title('Count Plot')
-    sns.countplot(titanic['Survived'])
+    ax1.set_title('Count Plot for Survival Status')
+    ax1=sns.countplot(titanic['Survived'])
     st.pyplot(fig1)
-    
+    st.markdown(body='''
+      <strong>Observation(s):-</strong> <span style="font">Number of passengers survived are lesser than the number of passengers died.</span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
+
+
+    fig7,ax7=plt.subplots()
+    ax7.set_title('Survival Status vs Male/Female')
+    ax7 = sns.countplot(x="Survived", hue="Sex", data=titanic)
+    st.pyplot(fig7)
+    st.markdown(body='''
+      <strong>Observation(s):-</strong> <span style="font">
+        <ul>
+        <li>Passengers who survived consists of more female passengers than male passengers.</li>
+        <li>Passengers who died had larger number of males as compared to females.</li>
+        </ul>
+      </span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
+
+
+    fig5,ax5=plt.subplots()
+    ax5.set_title('Count Plot for Passenger Class')
+    ax5=sns.countplot(x="Pclass", data=titanic)
+    st.pyplot(fig5)
+    st.markdown(body='''
+      <strong>Observation(s):-</strong>
+      <ul>
+        <li>Highest number of passangers belonged to Class 3.</li>
+        <li>Class 1 and Class 2 had almost same number of passengers.</li>
+      </ul>
+      </span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
+
+    fig6,ax6=plt.subplots()
+    ax6.set_title('Class vs Male/Female')
+    ax6 = sns.countplot(x="Pclass", hue="Sex", data=titanic)
+    st.pyplot(fig6)
+    st.markdown(body='''
+      <strong>Observation(s):-</strong> <span style="font">
+        <ul>
+        <li>Class 3 had a comparatively larger number of male passengers than female passengers.</li>
+        <li>The difference in the number of male and female passengers was not much pronounced in Class 1 and Class 2.</li>
+        </ul>
+      </span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
+
     fig2,ax2=plt.subplots()
-    ax2.set_title('Bar Plot')
-    sns.barplot(x='Pclass', y='Survived', data=titanic)
-    st.pyplot(fig2)
-    
+    ax2=sns.catplot(data=titanic, kind="swarm", x="Pclass", y="Age", hue="Survived") 
+    st.pyplot(ax2)
+    st.markdown(body='''
+    <strong>Observation(s):-</strong> <span style="font">
+      <ul>
+      <li>Class 3 had the highest number of young passengers followed by Class 2 and then Class 1.</li>
+      <li>Class 1 had most of the old passengers followed by Class 2 and then Class 3.</li>
+      </ul>
+    </span>    
+    <br>
+    <br>
+  ''', unsafe_allow_html=True)
+
     fig3,ax3=plt.subplots()
-    ax3.set_title('Fair vs Class')
-    sns.boxplot(x='Pclass', y='Fare', data=titanic)
+    ax3.set_title('Class vs Chance of Survival')
+    ax3=sns.barplot(x='Pclass', y='Survived', data=titanic)
     st.pyplot(fig3)
+    st.markdown(body='''
+      <strong>Observation(s):-</strong> <span style="font">The chances of survival were highest for people of higher class and the chances reduced as the class Reduced.</span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
+
+
+    fig4,ax4=plt.subplots()
+    ax4.set_title('Fair vs Class')
+    ax4=sns.boxplot(x='Pclass', y='Fare', data=titanic)
+    st.pyplot(fig4)
+    st.markdown(body='''
+      <strong>Observation(s):-</strong> <span style="font">Fair was highest for Class 1 and comparatively lesser for Class 2 and least for Class 3.</span>
+      <br>
+      <br>
+    ''', unsafe_allow_html=True)
 
 
 st.write('\n')
@@ -134,7 +221,6 @@ def models(X_train,Y_train):
   from sklearn.linear_model import LogisticRegression
   log = LogisticRegression(random_state=3)
   log.fit(X_train, Y_train)
-  
   #Using KNeighborsClassifier Method of neighbors class to use Nearest Neighbor algorithm
   from sklearn.neighbors import KNeighborsClassifier
   knn = KNeighborsClassifier(n_neighbors = 5)
@@ -181,12 +267,21 @@ def confusionMatrix(model, name):
 def make_prediction(query,model):
   predict=model.predict(query)
   if(predict):
-    st.header("Congratulations! You would have survived.")
+    st.markdown(body='''
+      <br>
+    	<div style='font-size:100px; text-align:center;'>&#128512;</div>	
+      <div style='text-align:center;'><h2>Congratulations!! You would have Survived.</h2></div>
+    ''', unsafe_allow_html=True)
   else:
-    st.header("Sorry! You wouldn't have Survived.")
+     st.markdown(body='''
+      <br>
+    	<div style='font-size:100px; text-align:center;'>&#128532;</div>	
+      <div style='text-align:center;'><h2>Sorry!! You Wouldn't have Survived.</h2></div>
+    ''', unsafe_allow_html=True)
 
 st.markdown(body='''
    <div style='border-bottom:2px Solid Black'></div>
+   <br>
 ''', unsafe_allow_html=True)
 
 #Training Models
@@ -199,31 +294,47 @@ data_load_state.text('Models Trained Successfully!!')
 #Show Performance of Models after Training
 if(st.checkbox('Show Performance of Models')):
   st.subheader('Performance of Various Models on Same Dataset-')
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('1.Logistic Regression Training Accuracy:', log.score(X_train, Y_train))
   confusionMatrix(log, "Logistic Regression")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+
   st.write('2. K Nearest Neighbor Training Accuracy:', knn.score(X_train, Y_train))
-  confusionMatrix(knn,"K Nearest Neighbor Training Accuracy")
+  confusionMatrix(knn,"K Nearest Neighbor")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('3. Support Vector Machine (Linear Classifier) Training Accuracy:', svc_lin.score(X_train, Y_train))
   confusionMatrix(svc_lin,"Support Vector Machine (Linear Classifier)")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('4. Support Vector Machine (RBF Classifier) Training Accuracy:', svc_rbf.score(X_train, Y_train))
   confusionMatrix(svc_rbf,"Support Vector Machine (RBF Classifier)")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('5. Gaussian Naive Bayes Training Accuracy:', gauss.score(X_train, Y_train))
   confusionMatrix(gauss,"Gaussian Naive Bayes")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('6. Decision Tree Classifier Training Accuracy:', tree.score(X_train, Y_train))
   confusionMatrix(tree,"Decision Tree Classifier")
+  st.markdown(body="<br>",unsafe_allow_html=True)
+  
   st.write('7. Random Forest Classifier Training Accuracy:', forest.score(X_train, Y_train))
   confusionMatrix(forest,"Random Forest Classifier")
   st.markdown(body='''
-  <h4><em>Random Forest is perfotming best for this dataset.</em></h4><br><br>
+  <br> 
+  <h3><em>Random Forest is performing best for this dataset.</em></h3><br><br><br>
   ''', unsafe_allow_html=True)
 
-if(st.checkbox('Show Importance of Features in Predicting the Survival Status of Passengers')):
+if(st.checkbox('Show Importance of features in Predicting the Survival Status of Passengers')):
   importances = pd.DataFrame({'feature':titanic.iloc[:, 1:8].columns,'Importance':np.round(forest.feature_importances_,3)})
   importances = importances.sort_values('Importance',ascending=False).set_index('feature')
   importances
 
 st.markdown(body='''
-   <div style='border-bottom:2px Solid Black'></div>
+  <br>
+  <div style='border-bottom:2px Solid Black'></div>
 ''', unsafe_allow_html=True)
 
 #Making Predictions 
